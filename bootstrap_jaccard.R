29ef4c1712461daeeb0d55ed875e5f21eaa7d7fb
Stephan Gade <stephan.gade@posteo.de>
2021-10-25 02:13:42 +0200

    Smaller changes.
    Fix spelling errors in function names and rename basic Jaccard index function. Fix doc string for resampling function and put plot and test code in extra function.

--- bootstrap_jaccard.R
+++ bootstrap_jaccard.R
@@ -1,7 +1,51 @@
-jc <- function(set1, set2) {
+#' Helper function to calculate the Jaccard index from two sets
+#' 
+#' @param set1 vector with elements for set 1
+#' @param set2 vector with elements for set2
+#' 
+#' @return the Jaccard index for the two sets
+jaccard <- function(set1, set2) {
   length(intersect(set1, set2)) / length(union(set1, set2))
 } 
 
+
+#' Helper function to calculate a p-value based on a statistic and a sampling
+#' based null distribution
+#' 
+#' @param x the statistic to be tested: only one value
+#' @param y vector with the sampling based null distribution
+#' @param alternative a character string specifying the alternative hypothesis
+#' @param plot boolean indicating whether the empirical distriubtion function
+#'               used to calculate the p-value should be plotted
+#' @param ... additional argument to plot for the cdf 
+#' 
+#' @return the p-value from the test
+resample_test <- function(x, y, 
+                          alternative = c("two.sides", "less", "greater"), 
+                          plot = F,
+                          ...) {
+  
+  alternative = match.arg(alternative)
+  x <- x[1]
+  
+  cdf <- ecdf(y)
+  
+  if (plot) {
+    plot(cdf, ...)
+    lims <- par("usr")
+    points(x, cdf(x), pch = 20, col = 'red')
+    segments(lims[1], cdf(x), x, cdf(x), col = 'red', lty = 2)
+    segments(x, cdf(x), x, lims[3], col = 'red', lty = 2)
+  }
+  
+  ## calculate p-value
+  p <- switch(alternative,
+              two.sided = 2 * min(cdf(x), 1 - cdf(x)),
+              less = cdf(x),
+              greater = 1 - cdf(x))
+  
+  p
+}
 
 #' Function to calculate the bootstrap estimate of the Jaccard Index of two sets
 #' 
@@ -14,7 +58,7 @@ jc <- function(set1, set2) {
 #'         1) jc_mean: the mean Jaccard index from the bootstrap samples
 #'         2) jc_se: the bootstrap standard error for the Jaccard Index
 #'         3) jc_samples: a vector with Jaccard Indices calculated from all bootstrap samples
-jarccard_bs <- function(set1, set2, num_bs = 500, seed = 123) {
+jaccard_bs <- function(set1, set2, num_bs = 500, seed = 123) {
   
   set.seed(seed)
   
@@ -48,10 +92,13 @@ jarccard_bs <- function(set1, set2, num_bs = 500, seed = 123) {
 #' @param seed seed for random number generator, default to 123
 #' 
 #' @return a list with 3 entries
-#'         1) jc_mean: the mean Jaccard index from the bootstrap samples
-#'         2) jc_se: the bootstrap standard error for the Jaccard Index
-#'         3) jc_samples: a vector with Jaccard Indices calculated from all bootstrap samples
-jarccard_resample <- function(set1, set2, n, nsampling = 500, seed = 123) {
+#'         1) jc_mean: the mean Jaccard index from all re-sampling runs
+#'         2) jc_sd: the accard index sd from all re-sampling runs
+#'         3) jc_se: the resampling standard error for the Jaccard Index
+#'         4) jc_med: the median Jaccard index from all re-sampling runs
+#'         5) jc_mad: the index MAD from all re-sampling runs
+#'         6) jc_samples: a vector with Jaccard Indices calculated from all bootstrap samples
+jaccard_resample <- function(set1, set2, n, nsampling = 500, seed = 123) {
   
   set.seed(seed)
   
@@ -88,24 +135,20 @@ jarccard_resample <- function(set1, set2, n, nsampling = 500, seed = 123) {
 ## define sets and and calculate Jaccard index
 set1 <- LETTERS[1:6]
 set2_orig <- LETTERS[5:11]
-jc_orig <- jc(set1, set2_orig)
+jc_orig <- jaccard(set1, set2_orig)
 
 ## calculate resampled Jaccard index based on random samples from all letters
-jc_resampling <- jarccard_resample(set1, set2 = LETTERS, 
-                                    n = length(set2_orig), 
-                                    nsampling = 10000, 
-                                    seed = 123)
+jc_resampl<- jaccard_resample(set1, set2 = LETTERS, 
+                              n = length(set2_orig), 
+                              nsampling = 10000, 
+                              seed = 353)
 
-## get ECDF from samples JC indices
-## and plot it
-jc_cdf <- ecdf(jc_resampling$jc_samples)
-
-plot(jc_cdf, main = "", xlab = 'sampled Jaccard indices', ylab = "empirical CDF")
-lims <- par("usr")
-points(jc_orig, jc_cdf(jc_orig), pch = 20, col = 'red')
-segments(lims[1], jc_cdf(jc_orig), jc_orig, jc_cdf(jc_orig), col = 'red', lty = 2)
-segments(jc_orig, jc_cdf(jc_orig), jc_orig, lims[3], col = 'red', lty = 2)
-
-## calculate one-sided p-value from ECDF 
-p <- 1-jc_cdf(jc_orig)
-message("Resampling p-value: ", p)
+## test the Jaccard index aggainst the sampled distribution of random Jaccard
+## indices. Test if the original index is higher than random sampling
+p <- resample_test(x = jc_orig, y = jc_resampl$jc_samples, 
+                   alternative = "greater",
+                   plot = T, 
+                   main = "Jaccard index ECDF", 
+                   xlab = "random sample indices", 
+                   ylab = "empirical distribution function")
+message("p-value from re-sampling based test: ", signif (p, 3))
