7c218f796a2530a246a5becb2625588dbe76af31
Stephan Gade <stephan.gade@posteo.de>
2021-11-01 16:03:17 +0100

    Major changes to teh Jaccard index resampling function.
    Fix an error in sampling and calculation the Jaccard index from the samples.
    Sample only from the null set and use two matrices to hold the intersect
    and the union. Rename some function arguments to make it more consistent and
    add comments. Add benchmark to compare vectorized version of resampling function
    with normal sapply.

--- bootstrap_jaccard.R
+++ bootstrap_jaccard.R
@@ -1,3 +1,8 @@
+#' @name bootsttrap_jaccard.R
+#' @version 0.1.1
+#' @author Stephan Gade
+
+
 #' Helper function to calculate the Jaccard index from two sets
 #' 
 #' @param set1 vector with elements for set 1
@@ -50,15 +55,15 @@ resample_test <- function(x, y,
 #' Function to calculate the bootstrap estimate of the Jaccard Index of two sets
 #' 
 #' @param set1 vector with all elements of first set
-#' @param set2 vectpr with all elements of second set
-#' @param num_bs number of bootstrap samples, default to 500
+#' @param set2 vector with all elements of second set
+#' @param n_samples number of bootstrap samples, default to 500
 #' @param seed seed for random number generator, default to 123
 #' 
 #' @return a list with 3 entries
 #'         1) jc_mean: the mean Jaccard index from the bootstrap samples
 #'         2) jc_se: the bootstrap standard error for the Jaccard Index
 #'         3) jc_samples: a vector with Jaccard Indices calculated from all bootstrap samples
-jaccard_bs <- function(set1, set2, num_bs = 500, seed = 123) {
+jaccard_bs <- function(set1, set2, n_samples = 500, seed = 123) {
   
   set.seed(seed)
   
@@ -83,39 +88,70 @@ jaccard_bs <- function(set1, set2, num_bs = 500, seed = 123) {
 }
 
 
-#' Function to calculate the bootstrap estimate of the Jaccard Index of two sets
+#' Function to calculate a Null distribution for the Jaccard index based on
+#' random sampling from a Null set. 
 #' 
+#' @details The function can be used to calculate the Jaccard index between 
+#' a fixed set1 and random samplings from a set of items (the Null set) to
+#' calculate a Null distribution of random Jaccard indices. This Null
+#' distribution can then be used to test a Jaccard index of set1 against a real
+#' set2.
+#'  
 #' @param set1 vector with all elements of first set
-#' @param set2 vector with all elements of second set
-#' @param n number of items to choose from set2
-#' @param nsamples number of sampling runs, default to 500
+#' @param null_set vector with all elements of the null set
+#' @param n_items number of items to choose from the null set
+#' @param n_samples number of sampling runs, default to 500
 #' @param seed seed for random number generator, default to 123
 #' 
-#' @return a list with 3 entries
+#' @return a list with following entries
 #'         1) jc_mean: the mean Jaccard index from all re-sampling runs
 #'         2) jc_sd: the accard index sd from all re-sampling runs
 #'         3) jc_se: the resampling standard error for the Jaccard Index
 #'         4) jc_med: the median Jaccard index from all re-sampling runs
 #'         5) jc_mad: the index MAD from all re-sampling runs
 #'         6) jc_samples: a vector with Jaccard Indices calculated from all bootstrap samples
-jaccard_resample <- function(set1, set2, n, nsampling = 500, seed = 123) {
+jaccard_resample <- function(set1, null_set, n_items, n_samples = 500, seed = 123) {
   
   set.seed(seed)
   
-  if (n >= length(set2)) {
-    stop("Number of items to sample n has to be smaller than number of items in set 2!")
+  if (n_items >= length(null_set)) {
+    stop("Number of items to sample 'n_items' has to be smaller than number of items in null set!")
   } 
  
-  union_set <- union(set1, set2)
-  intersect_set <- intersect(set1, set2)
-  set1_ex <- setdiff(set1, set2)
+  # calculate the union and intersect of the set of interest (set1) 
+  # and the Null set
+  union_set <- union(set1, null_set)
+  intersect_set <- intersect(set1, null_set)
   
-  samples <- lapply(1:nsampling, function(i) c(set1_ex, sample(set2, n, replace = F)))
-  samples <- sapply(samples, function(xx) union_set %in% xx)
-  rownames(samples) <- union_set
-
-  jaccard <- colSums(samples[intersect_set, ]) / colSums(samples)
+  # Draw random samples from the Null set, number of samples to draw is 
+  # specified by n_samples. We will draw without replacement.
+  sampling_list <- lapply(1:n_samples, function(i) sample(null_set, n_items, replace = F))
+   
+  # Calculate two matrices: 
+  #   - s_union: matrix of 0 and 1 with rows the items in the union set and
+  #              columns the sampling runs containing a 1 if an item ie either
+  #              part of set1 or of the corresponding random sample from the 
+  #              Null set, 0 otherwise
+  #   - s_intersect: matrix of 0 and 1 with rows corresponding to the items
+  #                  in the intersect between set1 and the Null set and columns
+  #                  to the sampling runs. Here, 1 indicates an item from the
+  #                  intersect set was present in the random sample from the Null
+  #                  set and 0 otherwise.
+  s_union     <- sapply(sampling_list, function(xx) union_set %in% c(set1, xx))
+  s_intersect <- sapply(sampling_list, function(xx) intersect_set %in% xx)
+ 
+  # Building up the two matrices from the list of random samplings from the Null
+  # set, the Jaccard index between set1 and each random sample can now simply be
+  # calculated by the element-wise division of two column sums. The first column
+  # sum is calculated for the matrix holding only items from the intersect set
+  # returning the intersection of set1 and all sampling runs from the Null set. 
+  # Consequently the second column sum returns the union of set1 and all samples
+  # from the Null set. So, the element-wise divsion of these tow column sums 
+  # gives the vectors of Jaccard indices from all sampling runs.
+  jaccard <- colSums(s_intersect) / colSums(s_union)
   
+  # Assemble the result list with the samples Jaccard indices and some
+  # summary statistics.
   res <- list(
     jc_mean = mean(jaccard),
     jc_sd = sd(jaccard),
@@ -133,14 +169,16 @@ jaccard_resample <- function(set1, set2, n, nsampling = 500, seed = 123) {
 # Test with simulated data
 
 ## define sets and and calculate Jaccard index
-set1 <- LETTERS[1:6]
-set2_orig <- LETTERS[5:11]
-jc_orig <- jaccard(set1, set2_orig)
+# set1 <- LETTERS[1:6]
+set1 <- c("1", "2", "3", "A", "B", "E", "F", "R")
+set2 <- LETTERS[5:11]
+jc_orig <- jaccard(set1, set2)
 
 ## calculate resampled Jaccard index based on random samples from all letters
-jc_resampl<- jaccard_resample(set1, set2 = LETTERS, 
-                              n = length(set2_orig), 
-                              nsampling = 10000, 
+jc_resampl<- jaccard_resample(set1 = set1, 
+                              null_set = LETTERS, 
+                              n_items = length(set2), 
+                              n_samples = 10000, 
                               seed = 353)
 
 ## test the Jaccard index aggainst the sampled distribution of random Jaccard
@@ -152,3 +190,16 @@ p <- resample_test(x = jc_orig, y = jc_resampl$jc_samples,
                    xlab = "random sample indices", 
                    ylab = "empirical distribution function")
 message("p-value from re-sampling based test: ", signif (p, 3))
+
+
+
+# Run time test
+library(microbenchmark)
+n_samples <- 1000
+microbenchmark(
+  jc_standard = ({
+    set.seed(123)
+    jc <- sapply(1:n_samples, function(i) jaccard(set1, sample(LETTERS, length(set2), replace = F)))
+  }),
+  jc_resampl = jaccard_resample(set1, null_set = LETTERS, n_items = length(set2), n_samples = n_samples, seed = 123)
+)
